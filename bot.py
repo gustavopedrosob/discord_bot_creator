1f70a0f8f27c8e489e194513d6d7c672e3699234
Gustavo <thevicio27@gmail.com>
2025-01-08 10:09:14 -0300

    refactor: bot.py

--- bot.py
+++ bot.py
@@ -25,10 +25,10 @@ class Bot:
             logger.info("Bot iniciado!")
 
         @self.client.event
-        async def on_message(message):
+        async def on_message(message: discord.Message):
             logger.info(f'Identificada mensagem "{message.content}".')
 
-            for message_name, message_data in messages.content().items():
+            for message_data in messages.content().values():
                 await message_and_reply(
                     message=message,
                     conditions=message_data.get("conditions"),
@@ -46,91 +46,84 @@ class Bot:
 
         @self.client.event
         async def apply_delay(delay):
-            if delay:
-                delay = int(delay)
-                logger.info(
-                    f"Aguardando delay de {delay} segundos para a proxima execução!"
+            delay = int(delay)
+            logger.info(
+                f"Aguardando delay de {delay} segundos para a proxima execução!"
+            )
+            await asyncio.sleep(delay)
+
+        @self.client.event
+        async def send_reaction(reactions, message: discord.Message, where: str):
+            for reaction in reactions:
+                code_reaction = reaction
+                reaction = (
+                    random_choose(reaction)
+                    if isinstance(reaction, list)
+                    else reaction
                 )
-                await asyncio.sleep(delay)
-
-        @self.client.event
-        async def send_reaction(reactions, message: discord.Message, where):
-            if reactions:
-                for reaction in reactions:
-                    code_reaction = reaction
-                    reaction = (
-                        random_choose(reaction)
-                        if isinstance(reaction, list)
-                        else reaction
-                    )
-                    reaction = emoji.emojize(reaction)
-                    try:
-                        if where == "author":
-                            await message.add_reaction(reaction)
-                        elif where == "bot" and message.author.bot:
-                            await message.add_reaction(reaction)
-                        logger.info(
-                            f'Adicionando a reação "{code_reaction}" a mensagem "{emoji.demojize(message.content)}" do autor {message.author}.'
-                        )
-                    except discord.HTTPException:
-                        print(reaction)
-
-        @self.client.event
-        async def send_reply(replies, message: discord.Message, where):
-            if replies:
-                for reply in replies:
-                    reply = random_choose(reply) if isinstance(reply, list) else reply
-                    reply = Variable(message).apply_variable(reply)
-                    if where == "group":
-                        await message.channel.send(reply)
-                    elif where == "private":
-                        dm_channel = await message.author.create_dm()
-                        await dm_channel.send(reply)
+                reaction = emoji.emojize(reaction)
+                try:
+                    if where == "author":
+                        await message.add_reaction(reaction)
+                    elif where == "bot" and message.author.bot:
+                        await message.add_reaction(reaction)
                     logger.info(
-                        f'Enviando a resposta "{reply}" à mensagem "{emoji.demojize(message.content)}" do autor {message.author}.'
+                        f'Adicionando a reação "{code_reaction}" a mensagem "{emoji.demojize(message.content)}" do autor {message.author}.'
                     )
+                except discord.HTTPException:
+                    print(reaction)
 
         @self.client.event
-        async def remove_message(delete, message: discord.Message):
-            if delete and isinstance(message.channel, discord.GroupChannel):
-                await message.delete()
+        async def send_reply(replies, message: discord.Message, where: str):
+            for reply in replies:
+                reply = random_choose(reply) if isinstance(reply, list) else reply
+                reply = Variable(message).apply_variable(reply)
+                if where == "group":
+                    await message.channel.send(reply)
+                elif where == "private":
+                    dm_channel = await message.author.create_dm()
+                    await dm_channel.send(reply)
                 logger.info(
-                    f'Removendo mensagem "{emoji.demojize(message.content)}" do autor {message.author}.'
+                    f'Enviando a resposta "{reply}" à mensagem "{emoji.demojize(message.content)}" do autor {message.author}.'
                 )
 
         @self.client.event
-        async def pin_message(pin, message: discord.Message):
-            if pin:
-                await message.pin()
-                logger.info(
-                    f'Fixando mensagem "{emoji.demojize(message.content)}" do autor {message.author}.'
-                )
+        async def remove_message(message: discord.Message):
+            await message.delete()
+            logger.info(
+                f'Removendo mensagem "{emoji.demojize(message.content)}" do autor {message.author}.'
+            )
 
         @self.client.event
-        async def kick_member(kick, message: discord.Message):
-            if kick and isinstance(message.channel, discord.GroupChannel):
-                await message.author.kick()
-                logger.info(f'Expulsando jogador "{message.author.name}".')
+        async def pin_message(message: discord.Message):
+            await message.pin()
+            logger.info(
+                f'Fixando mensagem "{emoji.demojize(message.content)}" do autor {message.author}.'
+            )
 
         @self.client.event
-        async def ban_member(ban, message: discord.Message):
-            if ban and isinstance(message.channel, discord.GroupChannel):
-                await message.author.ban()
-                logger.info(f'Banindo jogador "{message.author.name}".')
+        async def kick_member(kick: bool, message: discord.Message):
+            await message.author.kick()
+            logger.info(f'Expulsando jogador "{message.author.name}".')
+
+        @self.client.event
+        async def ban_member(ban: bool, message: discord.Message):
+            await message.author.ban()
+            logger.info(f'Banindo jogador "{message.author.name}".')
 
         async def message_and_reply(
             message: discord.Message,
-            conditions,
-            expected_message,
-            reply,
-            reaction,
-            delete,
-            pin,
-            delay,
-            ban,
-            kick,
-            where_reply="group",
-            where_reaction="author",
+            conditions: list[str],
+            expected_message: str,
+            reply: list[str],
+            reaction: list[str],
+            delete: bool,
+            pin: bool,
+            delay: int,
+            ban: bool,
+            kick: bool,
+            where_reply: str = "group",
+            where_reaction: str = "author",
         ):
 
             message_condition = MessageConditions(
@@ -154,11 +147,16 @@ class Bot:
             logger.info(f"Verificando condições {conditions_to_confirm}")
 
             if all_condition_is_true:
-                await apply_delay(delay)
-                await send_reply(reply, message, where_reply)
-                await send_reaction(reaction, message, where_reaction)
-                await remove_message(delete, message)
-                await pin_message(pin, message)
+                if delay:
+                    await apply_delay(delay)
+                if reply:
+                    await send_reply(reply, message, where_reply)
+                if reaction:
+                    await send_reaction(reaction, message, where_reaction)
+                if delete:
+                    await remove_message(message)
+                if pin:
+                    await pin_message(message)
 
     def run(self):
         try:
