cdd4ff9e5c0d5d2095c32d99bf24e3122e058968
Gustavo <thevicio27@gmail.com>
2025-01-10 23:52:21 -0300

    update: responsive color icon

--- core/functions.py
+++ core/functions.py
@@ -1,5 +1,9 @@
 from datetime import datetime
 
+from PySide6.QtCore import QSettings
+from PySide6.QtGui import QPixmap, Qt, QPainter, QColor
+from PySide6.QtSvg import QSvgRenderer
+
 
 def random_choose(lista: list):
     from random import choice
@@ -22,3 +26,25 @@ def have_in(lista: list, string: str, reverse=False) -> bool:
 
 def get_time(string: str):
     return datetime.now().strftime(string)
+
+
+def is_dark_mode() -> bool:
+    settings = QSettings(
+        "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+        QSettings.Format.NativeFormat,
+    )
+    return settings.value("AppsUseLightTheme") == 0
+
+
+def change_svg_color(svg_path: str, color: str) -> QPixmap:
+    renderer = QSvgRenderer(svg_path)
+    image = QPixmap(renderer.defaultSize())
+    image.fill(Qt.GlobalColor.transparent)
+
+    painter = QPainter(image)
+    renderer.render(painter)
+    painter.setCompositionMode(QPainter.CompositionMode.CompositionMode_SourceIn)
+    painter.fillRect(image.rect(), QColor(color))
+    painter.end()
+
+    return image
--- interfaces/classes/qpassword.py
+++ interfaces/classes/qpassword.py
@@ -1,5 +1,8 @@
+from PySide6.QtCore import QEvent
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QFrame, QHBoxLayout, QLineEdit, QPushButton
+
+from core.functions import is_dark_mode, change_svg_color
 
 
 class QPassword(QFrame):
@@ -18,13 +21,32 @@ class QPassword(QFrame):
 
         self.set_password_hide(True)
 
+        self.installEventFilter(self)
+
+    def eventFilter(self, obj, event):
+        if event.type() == QEvent.Type.PaletteChange:
+            self.__set_icon()
+        return super().eventFilter(obj, event)
+
     def is_password_hide(self) -> bool:
         return self.line_edit.echoMode() == QLineEdit.EchoMode.Password
+
+    def __set_icon(self) -> None:
+        hide = self.is_password_hide()
+        icon_path = (
+            "source/icons/eye-regular.svg"
+            if hide
+            else "source/icons/eye-slash-regular.svg"
+        )
+        if is_dark_mode():
+            image = change_svg_color(icon_path, "#FFFFFF")
+        else:
+            image = QIcon(icon_path)
+        self.hide_button.setIcon(image)
 
     def set_password_hide(self, hide: bool) -> None:
         if hide:
             self.line_edit.setEchoMode(QLineEdit.EchoMode.Password)
-            self.hide_button.setIcon(QIcon("source/icons/eye-regular.svg"))
         else:
             self.line_edit.setEchoMode(QLineEdit.EchoMode.Normal)
-            self.hide_button.setIcon(QIcon("source/icons/eye-slash-regular.svg"))
+        self.__set_icon()
