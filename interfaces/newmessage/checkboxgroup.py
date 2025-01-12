a519892ded8175f1f9b69d0a9b0e37f6ad76dfc4
Gustavo <thevicio27@gmail.com>
2025-01-12 00:28:05 -0300

    fix: keeping check states on save

--- interfaces/newmessage/checkboxgroup.py
+++ interfaces/newmessage/checkboxgroup.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QGridLayout, QWidget, QLabel, QCheckBox
 
 
@@ -13,22 +14,25 @@ class QCheckBoxGroup(QWidget):
         self.setLayout(self.__layout)
 
     def add_checkbox(self, name: str, checkbox: QCheckBox):
-        checkbox.clicked.connect(lambda r: self.__checkbox_clicked(name, checkbox))
+        checkbox.checkStateChanged.connect(
+            lambda check_state: self.__checkbox_checked(name, checkbox, check_state)
+        )
         self.__layout.addWidget(checkbox, len(self.__checkboxes), 1)
         self.__checkboxes[name] = checkbox
 
-    def __checkbox_clicked(self, name: str, checkbox: QCheckBox):
-        if name == self.__current_name:
-            self.__current.setChecked(False)
-            self.__current = None
-            self.__current_name = None
-        else:
-            self.__current = checkbox
-            self.__current_name = name
-            for r in self.__checkboxes.values():
-                if r != checkbox:
-                    r.setChecked(False)
-            self.__current.setChecked(True)
+    def __checkbox_checked(self, name: str, checkbox: QCheckBox, check_state: int):
+        if check_state == Qt.CheckState.Checked:
+            if name == self.__current_name:
+                self.__current.setChecked(False)
+                self.__current = None
+                self.__current_name = None
+            else:
+                self.__current = checkbox
+                self.__current_name = name
+                for r in self.__checkboxes.values():
+                    if r != checkbox:
+                        r.setChecked(False)
+                self.__current.setChecked(True)
 
     def get_current_name(self) -> str:
         return self.__current_name
