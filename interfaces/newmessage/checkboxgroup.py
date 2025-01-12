e047a4943b8158541ffe6c76b823df13f97588c7
Gustavo <thevicio27@gmail.com>
2025-01-12 01:32:18 -0300

    fix: saving unchecking

--- interfaces/newmessage/checkboxgroup.py
+++ interfaces/newmessage/checkboxgroup.py
@@ -22,17 +22,15 @@ class QCheckBoxGroup(QWidget):
 
     def __checkbox_checked(self, name: str, checkbox: QCheckBox, check_state: int):
         if check_state == Qt.CheckState.Checked:
-            if name == self.__current_name:
-                self.__current.setChecked(False)
-                self.__current = None
-                self.__current_name = None
-            else:
-                self.__current = checkbox
-                self.__current_name = name
-                for r in self.__checkboxes.values():
-                    if r != checkbox:
-                        r.setChecked(False)
-                self.__current.setChecked(True)
+            self.__current = checkbox
+            self.__current_name = name
+            for r in self.__checkboxes.values():
+                if r != checkbox:
+                    r.setChecked(False)
+            self.__current.setChecked(True)
+        elif name == self.__current_name:
+            self.__current = None
+            self.__current_name = None
 
     def get_current_name(self) -> str:
         return self.__current_name
