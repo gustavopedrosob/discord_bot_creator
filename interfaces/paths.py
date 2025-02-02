e4c7fa2ce338fa6dfc79ba1ca0f12ced51728105
Gustavo <thevicio27@gmail.com>
2025-02-02 19:59:30 -0300

    refactor: removing unused files

--- interfaces/paths.py
+++
@@ -1,7 +0,0 @@
-source = "source/"
-img = source + "icons/"
-message_and_reply = source + "message and reply.json"
-config = source + "config.json"
-bot = "bot.py"
-log = "log.txt"
-interface_logo = img + "logo.ico"
