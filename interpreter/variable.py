e654d18746f419688fd66520d58bcc5012f501f0
Gustavo <thevicio27@gmail.com>
2025-02-06 15:31:07 -0300

    update: improved variable application method

--- interpreter/variable.py
+++ interpreter/variable.py
@@ -6,21 +6,17 @@ from core.functions import get_time
 class Variable:
     def __init__(self, message: discord.Message):
         self.keys = {
-            "<author name>": message.author.name,  # if isinstance(message.channel, discord.GroupChannel) else message.author.nick
-            "<guild name>": (
+            "author name": message.author.name,
+            "guild name": (
                 message.guild.name
                 if isinstance(message.channel, discord.GroupChannel)
-                else None
+                else "N/A"
             ),
-            "<day>": get_time("%d"),
-            "<month>": get_time("%m"),
-            "<year>": get_time("%Y"),
-            "<d-m-y>": get_time("%d/%m/%Y"),
+            "day": get_time("%d"),
+            "month": get_time("%m"),
+            "year": get_time("%Y"),
+            "d-m-y": get_time("%d/%m/%Y"),
         }
 
     def apply_variable(self, string: str):
-        for key, value in self.keys.items():
-            if value is None:
-                continue
-            string = string.replace(key, value)
-        return string
+        return string.format(**self.keys)
